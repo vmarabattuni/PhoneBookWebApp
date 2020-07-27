5f6a7f5e7a6d3d904b77dd8e82e31143719f6aaf
Vamsi Krishna Marabattuni <vmarabattuni@pkglobal.com>
2020-07-27 14:39:01 +0530

    Master Search New Commit

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -37,6 +37,17 @@ namespace PhoneBookWebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index(FormCollection group)
         {
+            List<SelectListItem> listGroup = new List<SelectListItem>();
+            listGroup.Add(new SelectListItem { Text = "Email", Value = "Email" });
+            listGroup.Add(new SelectListItem { Text = "Phone", Value = "Phone" });
+            listGroup.Add(new SelectListItem { Text = "State", Value = "State" });
+            listGroup.Add(new SelectListItem { Text = "Country", Value = "Country" });
+            listGroup.Add(new SelectListItem { Text = "City", Value = "City" });
+            List<SelectListItem> listorderGroup = new List<SelectListItem>();
+            listorderGroup.Add(new SelectListItem { Text = "Ascending", Value = "Ascending" });
+            listorderGroup.Add(new SelectListItem { Text = "Descending", Value = "Descending" });
+            ViewBag.listDropDown = listGroup;
+            ViewBag.order = listorderGroup;
             if (!String.IsNullOrEmpty(group["selects"]))
             {
                 switch (group["selects"])
@@ -44,36 +55,118 @@ namespace PhoneBookWebApp.Controllers
                     case "Email":
                         if (!String.IsNullOrEmpty(group["Start"]))
                         {
-                            var start =group["Start"];
+                            var start = group["Start"];
                             var nr = group["count"];
                             if(group["order"] == "Ascending")
                             {
-                                var li = db.Peoples.Skip(start);
+                                var li = db.Peoples.Where( p => p.IsActive).Skip(int.Parse(start)-1).OrderBy( p => p.Email);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
                             }
                             else
                             {
-
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderByDescending(p => p.Email);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
                             }
                         }
                         else
                         {
-
+                            break;
                         }
-                        break;
+                        
                     case "Phone":
-                        Console.WriteLine("Today is Sunday.");
-                        break;
+                        if (!String.IsNullOrEmpty(group["Start"]))
+                        {
+                            var start = group["Start"];
+                            var nr = group["count"];
+                            if (group["order"] == "Ascending")
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderBy(p => p.PhoneNumber);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                            else
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderByDescending(p => p.PhoneNumber);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
                     case "State":
-                        Console.WriteLine("Today is Sunday.");
-                        break;
+                        if (!String.IsNullOrEmpty(group["Start"]))
+                        {
+                            var start = group["Start"];
+                            var nr = group["count"];
+                            if (group["order"] == "Ascending")
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderBy(p => p.StateId);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                            else
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderByDescending(p => p.StateId);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                       
                     case "Country":
-                        Console.WriteLine("Today is Sunday.");
-                        break;
+                        if (!String.IsNullOrEmpty(group["Start"]))
+                        {
+                            var start = group["Start"];
+                            var nr = group["count"];
+                            if (group["order"] == "Ascending")
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderBy(p => p.CountryId);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                            else
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderByDescending(p => p.CountryId);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
                     case "City":
-                        Console.WriteLine("Today is Sunday.");
-                        break;
+                        if (!String.IsNullOrEmpty(group["Start"]))
+                        {
+                            var start = group["Start"];
+                            var nr = group["count"];
+                            if (group["order"] == "Ascending")
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderBy(p => p.CityId);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                            else
+                            {
+                                var li = db.Peoples.Where(p => p.IsActive).Skip(int.Parse(start) - 1).OrderByDescending(p => p.CityId);
+                                var nli = li.Take(int.Parse(nr));
+                                return View(nli);
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
 
                 }
+                return RedirectToAction("Index");
             }
 
 
@@ -113,7 +206,7 @@ namespace PhoneBookWebApp.Controllers
         public ActionResult Search(FormCollection groupByString)
         {
 
-         
+
             if (!String.IsNullOrEmpty(groupByString["select"]))
             {
 
@@ -129,22 +222,75 @@ namespace PhoneBookWebApp.Controllers
                 List<People> people = db.Peoples.Where(p => p.IsActive).ToList();
 
 
-
-
-                var peopleGroup = db.Peoples.GroupBy(p => p.CityId).OrderBy( p => p.Key);
-
-                foreach (var g in peopleGroup)
+                switch (groupByString["select"])
                 {
-                    var cid = g.Key;
-                    foreach (var s in g)
-                    {
-                        List<People> speople = people.Where(p => p.CityId.Equals(cid)).ToList();
-                        people.Concat(speople);
-                    }
+                    case "City":
+                        var peoplecGroup = db.Peoples.GroupBy(p => p.CityId).OrderBy(p => p.Key);
+
+                        foreach (var g in peoplecGroup)
+                        {
+                            var cid = g.Key;
+                            foreach (var s in g)
+                            {
+                                List<People> speople = people.Where(p => p.CityId.Equals(cid)).ToList();
+                                people.Concat(speople);
+                            }
 
 
+                        }
+                        ViewBag.Count = people.Count();
+                        break;
+                        
+
+                    case "State":
+                        var peoplesGroup = db.Peoples.GroupBy(p => p.StateId).OrderBy(p => p.Key);
+
+                        foreach (var g in peoplesGroup)
+                        {
+                            var sid = g.Key;
+                            foreach (var s in g)
+                            {
+                                List<People> speople = people.Where(p => p.StateId.Equals(sid)).ToList();
+                                people.Concat(speople);
+                            }
+
+
+                        }
+                        ViewBag.Count = people.Count();
+                        break;
+                    case "Country":
+                        var peopleCGroup = db.Peoples.GroupBy(p => p.CountryId).OrderBy(p => p.Key);
+
+                        foreach (var g in peopleCGroup)
+                        {
+                            var Cid = g.Key;
+                            foreach (var s in g)
+                            {
+                                List<People> speople = people.Where(p => p.CountryId.Equals(Cid)).ToList();
+                                people.Concat(speople);
+                            }
+
+
+                        }
+                        ViewBag.Count = people.Count();
+                        break;
+                    case "Pin Code":
+                        var peoplepGroup = db.Peoples.GroupBy(p => p.PinCode).OrderBy(p => p.Key);
+
+                        foreach (var g in peoplepGroup)
+                        {
+                            var pid = g.Key;
+                            foreach (var s in g)
+                            {
+                                List<People> speople = people.Where(p => p.CountryId.Equals(pid)).ToList();
+                                people.Concat(speople);
+                            }
+
+
+                        }
+                        ViewBag.Count = people.Count();
+                        break;
                 }
-                ViewBag.Count = people.Count();
                 return View(people);
             }
     
